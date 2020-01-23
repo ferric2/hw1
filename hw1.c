88591aedaacd7e402f15d9bea3ca771268d79fca
ferric2 <59833849+ferric2@users.noreply.github.com>
2020-01-23 12:49:44 -0600

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "hw1.h"
+
+void sides_and(){
+	return;
+};
+
+int main(int argc,char* argv[]){
+  I_have_written();
+  the_code[0]=1;
+  that_you_needed();
+  and_which();
+  printf("ferric2\n%d",0);
+  return 0;
+}
+
+static int and_which(){
+  static int has_a_bunch_of = 1;
+  static int ridiculous = 1;
+  static double symbols;
+  return has_a_bunch_of + ridiculous + symbols;
+}
+
+void Forgive_me(){
+  return;
+}
+
+int they_are_arbitrary;
+double so_random = 20;
+
+void and_so_varied(){
+  return;
+}
---
+++ hw1.h
@@ -0,0 +1,12 @@
+static int I_have_written(){
+  return 0;
+}
+
+static int the_code[3] = {20,30,40};
+
+static int that_you_needed(){
+  static int to_compile = 1;
+  return to_compile;
+}
+
+static int and_which();
